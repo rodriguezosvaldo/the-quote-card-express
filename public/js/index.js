718bd3864759bc176a546aa28a7fff9db3b323cb
Osvaldo Rodriguez <osvaldo.rguez911001@gmail.com>
2025-06-18 08:57:25 -0400

    Using dotenv and cors to hide api key and restrict access to the endpoint respectively

--- public/js/index.js
+++ public/js/index.js
@@ -6,15 +6,14 @@ const elements = {
 };
 
 async function getRandomImage() {
-    const client_id = "sKwlLPYgu2AWBURsDHAxK0LGD7DU57YKkde7qRkkljI";
-    const endpoint = `https://api.unsplash.com/photos/random/?client_id=${client_id}`;
+    const endpoint = "http://localhost:8080/api/v1/getRandomImage";
     try {
         const response = await fetch(endpoint);
         const returnedData = await response.json();
-        const receivedPhotoUrl = returnedData.urls.regular;
+        const receivedPhotoUrl = returnedData.data;
 
         const imgDiv = document.querySelector(".background-img");
-        imgDiv.style.backgroundImage = `url("${receivedPhotoUrl}")`;
+        imgDiv.style.backgroundImage = `url(${receivedPhotoUrl})`;
     } catch (error) {
         console.error(error);
     }
